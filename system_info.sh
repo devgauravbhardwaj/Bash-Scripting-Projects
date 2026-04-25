242b3793d881455b85f3bce1435bf66994e0dff5
devgauravbhardwaj <dev.gauravbhardwaj@hotmail.com>
2026-04-25 18:22:34 +0530

    Docs: Update README with system_info.sh details

---
+++ system_info.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo :"Welcome to bash Script"
+
+echo :"##################################"
+
+echo "Uptime of the system is : "
+uptime
+
+
+echo :"#################################"
+
+echo "Memory Utilization"
+free -m
+
+echo :"#################################"
+
+echo "Disk Utilization"
+df 
+
+
